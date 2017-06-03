654b9821aa465ce6163551b52afb19c8f9866da3
Sebastian Körkkö <korkkosebastian@gmail.com>
2017-06-03 12:28:32 +0300

    Fixed clean build issue that ended in null pointer by adding call to set default values on PreferenceManager. Fixed wrong default ip address port to 20000

--- app/src/main/java/com/korkkosebastian/cwpclient/MainActivity.java
+++ app/src/main/java/com/korkkosebastian/cwpclient/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements CWPProvider {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
